d4c6202c4d961f84eb54e3142d4d6bce76cd9262
derFrickler <johannes@hermen.net>
2018-08-23 07:54:38 +0200

    modified to work with single trigger/echo pin.

--- src/HCSR04.cpp
+++ src/HCSR04.cpp
@@ -1,6 +1,7 @@
 /*
   HCSR04 - Library for arduino, for HC-SR04 ultrasonic distance sensor.
   Created by Martin Sosic, June 11, 2016.
+  modified by der-frickler to work with single trigger/echo pin.
 */
 
 #include "Arduino.h"
@@ -10,12 +11,13 @@ UltraSonicDistanceSensor::UltraSonicDistanceSensor(
         int triggerPin, int echoPin) {
     this->triggerPin = triggerPin;
     this->echoPin = echoPin;
-    pinMode(triggerPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+   // pinMode(triggerPin, OUTPUT);
+   // pinMode(echoPin, INPUT);
 }
 
 double UltraSonicDistanceSensor::measureDistanceCm() {
     // Make sure that trigger pin is LOW.
+    pinMode(triggerPin, OUTPUT);
     digitalWrite(triggerPin, LOW);
     delayMicroseconds(2);
     // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
@@ -23,6 +25,7 @@ double UltraSonicDistanceSensor::measureDistanceCm() {
     delayMicroseconds(10);
     digitalWrite(triggerPin, LOW);
     // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
+    pinMode(echoPin, INPUT);
     unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
     double distanceCm = durationMicroSec / 2.0 * 0.0343;
     if (distanceCm == 0 || distanceCm > 400) {
@@ -31,3 +34,4 @@ double UltraSonicDistanceSensor::measureDistanceCm() {
         return distanceCm;
     }
 }
+
